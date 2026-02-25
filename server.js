462cab97b640205c0638cdc211ec9b3841d1ed17
kgabor016 <konczgaborsandor6@gmail.com>
2026-02-25 10:07:41 +0100

    javitas

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ const path = require("path");
 
 const app = express();
 
-// Engedélyezés minden origin számára (biztonságosabb a teszthez)
+// CORS engedélyezése minden hálózatról
 app.use(cors({
-  origin: '*' // bárhonnan érkező kérést engedélyez
+  origin: '*' // bármely gép, bármilyen origin
 }));
 
 app.use(express.json());
 
 let utolsoAdat = {};
 
-// Statikus fájlok
+// Statikus fájlok kiszolgálása
 app.use(express.static(path.join(__dirname, "public")));
 
-// POST adatok fogadására
+// POST a küldő oldaltól
 app.post("/", (req, res) => {
   utolsoAdat = req.body;
   console.log("Kapott adat:", utolsoAdat);
   res.json({ ok: true });
 });
 
-// GET a display-nek
+// GET a display oldalnak
 app.get("/adat", (req, res) => {
   res.json(utolsoAdat);
 });
 
-// Hallgatás minden interfészen
+// Hallgatás minden hálózati interfészen (nem csak localhost)
 app.listen(5000, "0.0.0.0", () => {
-  console.log("Fut minden hálózaton: http://0.0.0.0:5000");
+  console.log("Szerver fut minden hálózaton: http://0.0.0.0:5000");
 });
